8dbbefaf8717beb3364d97cbbdd0b161466d619b
sarveshkg <sarveshkg@gmail.com>
2026-02-26 20:27:44 +0530

    Jwt error 2

--- supabase/functions/_shared/auth.ts
+++ supabase/functions/_shared/auth.ts
@@ -1,5 +1,6 @@
 
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
+
 
 // @ts-ignore
 declare const Deno: any;
